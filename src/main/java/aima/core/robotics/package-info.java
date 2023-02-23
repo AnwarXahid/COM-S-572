b60bb5bd3b758eda258f7210adad0b9edf0f6cb2
AnwarXahid <mdahzahid@gmail.com>
2023-02-23 04:30:07 -0600

    remove unused code!

--- src/main/java/aima/core/robotics/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * This package provides an generic apporach to the Monte-Carlo-Localization algorithm.<br/>
- * The sub-packages of {@code impl} provide functionality for a two-dimensional environment.
- */
-package aima.core.robotics;
